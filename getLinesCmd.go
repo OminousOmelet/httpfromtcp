a6e0414e1621460e408eddef37dc9d6a7562dc16
OminousOmelet <kittenthief@live.com>
2026-08-14 15:03:14 -0400

    listeners, connections, channels, oh my...

---
+++ getLinesCmd.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	strChan := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(strChan)
+		var msg string
+		var msgLine string
+
+		// adds 8 bytes at a time to a line, and sends line to channel on linebreaks
+		for {
+			buff := make([]byte, 8)
+			_, err := f.Read(buff)
+			if err != nil { //need to check for nil first so no panic?
+				if !errors.Is(err, io.EOF) {
+					fmt.Printf("Error reading file: %s", err)
+					return
+				}
+				break
+			}
+			msg += string(buff)
+			if strings.Contains(msg, "\n") {
+				parts := strings.Split(msg, "\n")
+				msgLine += parts[0]
+				strChan <- msgLine
+				msgLine = parts[1]
+				msg = ""
+			}
+		}
+	}()
+	return strChan
+}
--- main.go
+++ main.go
@@ -1,54 +1,29 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"os"
-	"strings"
+	"log"
+	"net"
 )
 
 func main() {
-	f, err := os.Open("messages.txt")
+	lsnr, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		fmt.Printf("Error opening file: %s", err)
-		os.Exit(1)
+		log.Fatalf("Error listening to network: %s", err)
 	}
-	strChan := getLinesChannel(f)
-	for str := range strChan {
-		fmt.Printf("read: %s\n", str)
-	}
-}
+	defer lsnr.Close()
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	strChan := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(strChan)
-		var msg string
-		var msgLine string
-
-		// adds 8 bytes at a time to a line, and sends line to channel on linebreaks
-		for {
-			buff := make([]byte, 8)
-			_, err := f.Read(buff)
-			if err != nil {
-				if !errors.Is(err, io.EOF) {
-					fmt.Printf("Error reading file: %s", err)
-					return
-				}
-				break
-			}
-			msg += string(buff)
-			if strings.Contains(msg, "\n") {
-				parts := strings.Split(msg, "\n")
-				msgLine += parts[0]
-				strChan <- msgLine
-				msgLine = parts[1]
-				msg = ""
-			}
+	for {
+		conn, err := lsnr.Accept()
+		if err != nil {
+			log.Fatalf("Error awaiting connection: %s", err)
 		}
-	}()
-	return strChan
+
+		fmt.Println("Accepted connection from", conn.RemoteAddr())
+		strChan := getLinesChannel(conn) // conn implements io...
+		for str := range strChan {
+			fmt.Println(str)
+		}
+		fmt.Println("Connection to", conn.RemoteAddr(), "closed")
+	}
 }
