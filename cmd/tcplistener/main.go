1a4ca409c43053d80ab1498e72964d0fdd4ee001
OminousOmelet <kittenthief@live.com>
2026-08-27 14:31:54 -0400

    testing not working temporarily, jumped the gun with channels...

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+	Req "httpfromtcp/internal/request"
 	"log"
 	"net"
 )
 
+const PORT = ":42069"
+
 func main() {
-	lsnr, err := net.Listen("tcp", ":42069")
+	lsnr, err := net.Listen("tcp", PORT)
 	if err != nil {
 		log.Fatalf("Error listening to network: %s", err)
 	}
 	defer lsnr.Close()
 
+	fmt.Println("Listening for TCP traffic on", PORT)
 	for {
 		conn, err := lsnr.Accept()
 		if err != nil {
@@ -20,10 +24,15 @@ func main() {
 		}
 
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
-		strChan := getLinesChannel(conn) // conn implements io...
-		for str := range strChan {
-			fmt.Println(str)
+		reqChan, err := Req.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("Error reading request: %s", err)
 		}
-		fmt.Println("Connection to", conn.RemoteAddr(), "closed")
+		req := <-reqChan
+		if req != nil {
+			fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+				req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion)
+		}
+		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 	}
 }
